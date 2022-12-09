6a8e768117ce74a0ab5a278097971e76349290a4
javckzack <javck.zack@gmail.com>
2022-12-09 14:24:15 +0800

    Exam 題目

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//請完成下方所有方法的實作，並撰寫對應的路由，用 Postman 來進行測試
 class ArticleController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * 回傳該表格的所有資料，以 sort 欄位從小到大排序
      *
      * @return \Illuminate\Http\Response
      */
@@ -18,7 +19,7 @@ class ArticleController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * 儲存前端傳入的資料，成功後回傳儲存的內容
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -29,7 +30,7 @@ class ArticleController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * 回傳指定的資料
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -40,7 +41,7 @@ class ArticleController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * 更新指定的資料，成功後回傳更新後的內容
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
@@ -52,7 +53,7 @@ class ArticleController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * 刪除指定的資料，成功後回傳 Success
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
@@ -60,5 +61,41 @@ class ArticleController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    //查詢創建於 2022/12/13 00:00:00 之後，enabled 為 true 的資料，按照 created_at 從新到舊排序，回傳第一筆資料的 subject 欄位內容
+    public function querySpecific()
+    {
+
+    }
+
+    //查詢創建於 2022/12/10 00:00:00 之後，enabled 為 true 的資料，按照 created_at 從新到舊排序，回傳第2~4筆資料
+    public function queryPagination()
+    {
+
+    }
+
+    //查詢創建日期介於 2022/12/10 00:00:00 和 2022/12/15 23:59:59 之間，sort 位於 $min 到 $max 之間的資料並回傳
+    public function queryRange($min,$max)
+    {
+
+    }
+
+    //根據所傳入的分類id，取出該分類所有 enabled 為 true 的資料，依照 sort 從小到大排序，回傳符合的資料
+    public function queryByCgy($cgy_id)
+    {
+
+    }
+
+    //試著使用 pluck() 來取得 id 為 key ， subject 為 value 的陣列
+    public function queryPluck()
+    {
+
+    }
+
+    //計算所有 enabled 為 true 的資料筆數後回傳，利用查詢方法 count()
+    public function enabledCount()
+    {
+
     }
 }
